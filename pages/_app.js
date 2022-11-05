59b708ca1b199528115bf0d0eb4b11127cb0f1bd
Andrés Gamba <xnovaerax@gmail.com>
2022-11-04 20:55:16 -0500

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-7MSB3S0DEM" />
+    <Script
+      id='google-analytics'
+      strategy='afterInteractive'
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+  
+        gtag('config', 'G-7MSB3S0DEM');`
+      }}
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
